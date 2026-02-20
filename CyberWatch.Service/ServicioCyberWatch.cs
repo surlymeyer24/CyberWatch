9380f6e3239a04f8ca3c898d5935bd1d7360e3cc
Surly Meyer <89383@sistemas.frc.utn.edu.ar>
2026-02-20 12:29:41 -0300

    Definicion de estructura sin FB

---
+++ CyberWatch.Service/ServicioCyberWatch.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace CyberWatch.Service
+{
+    public class ServicioCyberWatch : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+
+        {
+            // Implementar la lógica del servicio
+        }
+    }
+}
